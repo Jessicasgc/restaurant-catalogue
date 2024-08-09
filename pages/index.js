4772c5c58b468095a499c1bd7d6408bd9e62ce8f
Jessicasgc <jessicaamadearahma@gmail.com>
2024-08-10 03:11:05 +0700

    giving style with styled component

--- pages/index.js
+++ pages/index.js
@@ -2,14 +2,13 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import RestaurantsList from '../components/RestaurantsList';
+import Container from '../components/styled/Container';
 
 export default function Home({restaurants}) {
   return (
-    <div className='container'>
-      <div className='container'>
-        <RestaurantsList restaurants={restaurants} />
-      </div>
-    </div>
+    <Container maxWidth="80%">
+      <RestaurantsList restaurants={restaurants} />
+    </Container>
   )
 }
 export async function getStaticProps() {
